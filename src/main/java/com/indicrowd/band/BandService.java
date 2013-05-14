6a1062fda6c0aa84593b802840e77d637461a0f2
hansol <oksktank@gmail.com>
2013-05-14 18:14:11 +0900

    fix bug

--- src/main/java/com/indicrowd/band/BandService.java
+++ src/main/java/com/indicrowd/band/BandService.java
@@ -35,16 +35,6 @@ public class BandService {
 		for(int i=0; i<tags.length; i++){
 			tagService.inputTag(tags[i]);
 		}
-		
-		if (bandInfo.getProfilePhoto() != null && bandInfo.getProfilePhoto().getSize() > 0) {
-			fileService.save(bandInfo.getProfilePhoto(), "bandphoto/" + bandInfo.getId().toString(), true);
-			fileService.save(imageService.generateThumb(bandInfo.getProfilePhoto()), "bandthumb/" + bandInfo.getId().toString(), true);
-		} else {
-			File file = fileService.load("img/blankconcert.jpg");
-			fileService.save(file, "bandphoto/" + bandInfo.getId().toString(), true);
-			fileService.save(imageService.generateThumb(file), "bandthumb/" + bandInfo.getId().toString(), true);
-		}
-		
 		BandMember bandMember = new BandMember();
 		bandMember.setUserInfo(userInfo);
 		bandMember.setBandInfo(bandInfo);
@@ -56,6 +46,16 @@ public class BandService {
 			bandInfoList = new ArrayList<BandInfo>();
 		}
 		bandInfoList.add(bandInfo);
+		if (bandInfo.getProfilePhoto() != null && bandInfo.getProfilePhoto().getSize() > 0) {
+			fileService.save(bandInfo.getProfilePhoto(), "bandphoto/" + bandInfo.getId().toString(), true);
+			fileService.save(imageService.generateThumb(bandInfo.getProfilePhoto()), "bandthumb/" + bandInfo.getId().toString(), true);
+		} else {
+			File file = fileService.load("img/blankconcert.jpg");
+			fileService.save(file, "bandphoto/" + bandInfo.getId().toString(), true);
+			fileService.save(imageService.generateThumb(file), "bandthumb/" + bandInfo.getId().toString(), true);
+		}
+		
+		
 		
 		return bandInfo;
 	}
