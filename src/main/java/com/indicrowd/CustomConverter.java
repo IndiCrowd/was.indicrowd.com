28e24be80201b41374abb64b3fa54d27999a3c13
hanul <mr@hanul.co>
2013-07-04 19:13:56 +0900

    E

---
+++ src/main/java/com/indicrowd/CustomConverter.java
@@ -0,0 +1,44 @@
+package com.indicrowd;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.thoughtworks.xstream.converters.MarshallingContext;
+import com.thoughtworks.xstream.converters.collections.MapConverter;
+import com.thoughtworks.xstream.io.ExtendedHierarchicalStreamWriterHelper;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
+import com.thoughtworks.xstream.mapper.Mapper;
+
+public class CustomConverter extends MapConverter {
+
+	public CustomConverter(Mapper mapper) {
+		super(mapper);
+	}
+
+	@Override
+	@SuppressWarnings("rawtypes")
+	public boolean canConvert(Class type) {
+		return type.equals(HashMap.class) || type.equals(Hashtable.class) || type.getName().equals("java.util.LinkedHashMap") || type.getName().equals("java.util.concurrent.ConcurrentHashMap") || type.getName().equals("sun.font.AttributeMap");
+	}
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public void marshal(Object source, HierarchicalStreamWriter writer, MarshallingContext context) {
+		Map<String, Object> map = (Map<String, Object>) source;
+		for (Iterator<Entry<String, Object>> iterator = map.entrySet().iterator(); iterator.hasNext();) {
+			Entry<String, Object> entry = (Entry<String, Object>) iterator.next();
+
+			ExtendedHierarchicalStreamWriterHelper.startNode(writer, mapper().serializedClass(Entry.class), entry.getClass());
+
+			writeItem("@id", context, writer);
+			writeItem(entry.getKey(), context, writer);
+			writeItem(entry.getValue(), context, writer);
+
+			writer.endNode();
+		}
+	}
+
+}
